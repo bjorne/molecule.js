f58e6fe97b71f0f63e8d4389427143ea7660b8f9
Björn Ramberg <bjorn@burtcorp.com>
2012-09-05 19:57:53 +0200

    Add utility function lib.

--- examples/voltage-log.js
+++ examples/voltage-log.js
@@ -1,4 +1,4 @@
-module.exports = function($) {
+module.exports = function($, fn) {
   $({ type: 'BusVoltage' })
     .on('meta:voltage', function(unit, voltage) {
       console.log('voltage is', voltage);
